ae8800b5292b75ebfb475ebb023a09216564f027
Benjamin Warncke <benjamin.warncke@liveality.com>
2014-02-19 13:52:15 -0600

    Added campaign/list and reports/summary v2.0 method

---
+++ src/main/java/com/ecwid/mailchimp/method/v2_0/campaign/ListMethod.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2014 Ecwid, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ecwid.mailchimp.method.v2_0.campaign;
+
+import java.util.Date;
+
+import com.ecwid.mailchimp.MailChimpAPIVersion;
+import com.ecwid.mailchimp.MailChimpMethod;
+import com.ecwid.mailchimp.MailChimpObject;
+
+/**
+ * Get the list of campaigns and their details matching the specified filters
+ * 
+ * @author Benjamin Warncke
+ */
+@MailChimpMethod.Method(name = "campaigns/list", version = MailChimpAPIVersion.v2_0)
+public class ListMethod extends CampaignRelatedMethod<ListMethodResult> {
+
+	@Field
+	public Filters filters;
+
+	@Field
+	public Integer start;
+
+	@Field
+	public Integer limit;
+
+	@Field
+    public String sort_field;
+
+    @Field
+    public String sort_dir;
+
+    /**
+     * optional - a struct of filters to apply to this query - all are optional:
+     */
+    public static class Filters extends MailChimpObject {
+
+        @Field
+        public String campaign_id;
+
+        @Field
+        public String parent_id;
+
+        @Field
+        public String list_id;
+
+		@Field
+        public Integer folder_id;
+
+        @Field
+        public Integer template_id;
+
+		@Field
+        public String status;
+
+        @Field
+        public String type;
+
+        @Field
+        public String from_name;
+
+        @Field
+        public String from_email;
+
+        @Field
+        public String title;
+
+        @Field
+        public String subject;
+
+        @Field
+        public Date sendtime_start; //24 hour format in GMT, eg "2013-12-30 20:30:00"
+
+        @Field
+        public Date sendtime_end; //24 hour format in GMT, eg "2013-12-30 20:30:00"
+
+		@Field
+        public Boolean uses_segment;
+
+        @Field
+        public Boolean exact;
+    }
+	
+    @Override
+    public Class<ListMethodResult> getResultType() {
+        return ListMethodResult.class;
+    }
+
+}
---
+++ src/main/java/com/ecwid/mailchimp/method/v2_0/campaign/ListMethodResult.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright 2014 Ecwid, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ecwid.mailchimp.method.v2_0.campaign;
+
+import java.util.Date;
+import java.util.List;
+
+import com.ecwid.mailchimp.MailChimpObject;
+import com.ecwid.mailchimp.method.v2_0.reports.SummaryMethodResult;
+
+/**
+ * containing a count of all matching campaigns, the specific ones for the current page, and any errors from the filters provided
+ * 
+ * @author Benjamin Warncke
+ */
+public class ListMethodResult extends MailChimpObject {
+	
+	@Field
+    public Integer total;
+
+    @Field
+    public List<Data> data;
+
+    @Field
+    public List<Error> errors;
+
+    /**
+     * structs for each campaign being returned
+     */
+    public static class Data extends MailChimpObject {
+        @Field
+        public String id;
+
+        @Field
+        public Integer web_id;
+
+        @Field
+        public String list_id;
+
+        @Field
+        public Integer folder_id;
+
+        @Field
+        public Integer template_id;
+
+        @Field
+        public String content_type;
+        
+        @Field
+        public String content_edited_by;
+
+        @Field
+        public String title;
+
+        @Field
+        public String type;
+
+        @Field
+        public String create_time;
+
+        @Field
+        public String sent_time;
+        
+        @Field
+        public String content_updated_time;
+
+        @Field
+        public String status;
+
+        @Field
+        public String from_name;
+
+        @Field
+        public String from_email;
+
+        @Field
+        public String subject;
+
+        @Field
+        public String to_name;
+
+        @Field
+        public String archive_url;
+        
+        @Field
+        public String archive_url_long;
+        
+        @Field
+        public Integer emails_sent;
+
+        @Field
+        public Boolean inline_css;
+
+        @Field
+        public String analytics;
+
+        @Field
+        public String analytics_tag;
+
+        @Field
+        public Boolean authenticate;
+
+        @Field
+        public Boolean ecomm360;
+
+        @Field
+        public Boolean auto_tweet;
+
+        @Field
+        public String auto_fb_post;
+
+        @Field
+        public Boolean auto_footer;
+
+        @Field
+        public Boolean timewarp;
+
+        @Field
+        public String timewarp_schedule;
+        
+        @Field
+        public Tracking tracking;
+
+        @Field
+        public String parent_id;
+
+        @Field
+        public Boolean is_child;
+
+        @Field
+        public String tests_send;
+
+        @Field
+        public Integer tests_remain;
+
+        @Field
+        public String segment_text;
+
+        @Field
+        public SegmentOpts segment_opts;
+
+        @Field
+        public SavedSegment saved_segment;
+
+        @Field
+        public List<String> type_opts;
+
+        @Field
+        public Integer comments_total;
+
+        @Field
+        public Integer comments_unread;
+
+        @Field
+        public SummaryMethodResult summary;
+    }
+
+    /**
+     * the various tracking options used
+     */
+    public static class Tracking extends MailChimpObject{
+
+        @Field
+        public Boolean html_clicks;
+
+        @Field
+        public Boolean text_clicks;
+
+        @Field
+        public Boolean opens;
+    }
+    
+    /**
+     * the segment used for the campaign - can be passed to campaigns/segment-test or campaigns/create()
+     */
+    public static class SegmentOpts extends MailChimpObject {
+    	
+    	@Field
+        public String match;
+    	
+    	@Field
+    	public List<Conditions> conditions;
+    }
+    
+    /**
+     * the segment used for the campaign - can be passed to campaigns/segment-test or campaigns/create()
+     */
+    public static class Conditions extends MailChimpObject {
+    	
+    	@Field
+        public String field;
+
+        @Field
+        public String op;
+
+        @Field
+        public String value;
+    }
+
+    /**
+     * the various tracking options used
+     */
+    public static class SavedSegment extends MailChimpObject{
+
+        @Field
+        public Integer id;
+
+        @Field
+        public String type;
+        
+        @Field
+        public String name;
+    }
+
+    /**
+     * structs of any errors found while loading lists - usually just from providing invalid list ids
+     */
+    public static class Error extends MailChimpObject {
+        @Field
+        public String filter;
+
+        @Field
+        public String value;
+
+        @Field
+        public Integer code;
+
+        @Field
+        public String error;
+    }
+}
